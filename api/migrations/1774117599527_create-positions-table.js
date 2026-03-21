def5aa6e46343645c52357e3a96e235a2d65988d
A <alisazz805@gmail.com>
2026-03-21 21:41:33 +0300

    feat: добавить таблицу должностей

---
+++ api/migrations/1774117599527_create-positions-table.js
@@ -0,0 +1,56 @@
+/**
+ * @type {import('node-pg-migrate').ColumnDefinitions | undefined}
+ */
+export const shorthands = undefined;
+
+/**
+ * @param pgm {import('node-pg-migrate').MigrationBuilder}
+ * @param run {() => void | undefined}
+ * @returns {Promise<void> | void}
+ */
+export const up = (pgm) => {
+    pgm.createTable('positions', {
+        id: {
+            type: 'uuid',
+            primaryKey: true,
+            default: pgm.func('gen_random_uuid()')
+        },
+        name: { type: 'text', notNull: true },
+        comment: { type: 'text' },
+        created_at: {
+            type: 'timestamptz',
+            notNull: true,
+            default: pgm.func('current_timestamp'),
+        },
+        updated_at: {
+            type: 'timestamptz',
+            notNull: true,
+            default: pgm.func('current_timestamp'),
+        },
+        deleted_at: { type: 'timestamptz' },
+    });
+
+    pgm.createIndex('positions', 'name', {
+        unique: true,
+        where: 'deleted_at IS NULL',
+        name: 'unique_active_position_name'
+    });
+
+    pgm.sql(`
+        CREATE TRIGGER update_positions_updated_at
+        BEFORE UPDATE ON positions
+        FOR EACH ROW
+        EXECUTE PROCEDURE update_trig();
+    `);
+};
+
+/**
+ * @param pgm {import('node-pg-migrate').MigrationBuilder}
+ * @param run {() => void | undefined}
+ * @returns {Promise<void> | void}
+ */
+export const down = (pgm) => {
+    pgm.sql('DROP TRIGGER IF EXISTS update_positions_updated_at ON positions;');
+    pgm.dropIndex('positions', [], { name: 'unique_active_positions_name' });
+    pgm.dropTable('positions');
+};
