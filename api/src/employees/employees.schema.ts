2421d6e3b5732a9f10903838d9370d18912543ee
A <alisazz805@gmail.com>
2026-04-03 16:35:39 +0300

    refactor: улучшить валидацию фио

--- api/src/employees/employees.schema.ts
+++ api/src/employees/employees.schema.ts
@@ -1,17 +1,42 @@
 import * as Joi from 'joi';
-
+const namePattern = /^[а-яёa-z\s\-']+$/i;
 export const createEmployeeSchema = Joi.object({
-  last_name: Joi.string().min(2).max(100).required().messages({
-    'string.empty': 'Введите фамилию',
-    'any.required': 'Поле "фамилия" обязательно',
-  }),
-  first_name: Joi.string().min(2).max(100).required().messages({
-    'string.empty': 'Введите имя',
-    'any.required': 'Поле "имя" обязательно',
-  }),
-  middle_name: Joi.string().max(100).allow('', null).optional(),
+  last_name: Joi.string()
+    .trim()
+    .min(2)
+    .max(100)
+    .pattern(namePattern)
+    .required()
+    .messages({
+      'string.empty': 'Введите фамилию',
+      'string.pattern.base':
+        'Фамилия может содержать только буквы, дефис и апостроф',
+      'any.required': 'Поле "фамилия" обязательно',
+    }),
+  first_name: Joi.string()
+    .trim()
+    .min(2)
+    .max(100)
+    .pattern(namePattern)
+    .required()
+    .messages({
+      'string.empty': 'Введите имя',
+      'string.pattern.base':
+        'Имя может содержать только буквы, дефис и апостроф',
+      'any.required': 'Поле "имя" обязательно',
+    }),
+  middle_name: Joi.string()
+    .trim()
+    .max(100)
+    .pattern(namePattern)
+    .allow('', null)
+    .optional()
+    .messages({
+      'string.pattern.base':
+        'Отчество может содержать только буквы, дефис и апостроф',
+    }),
   birth_date: Joi.date().iso().max('now').required().messages({
-    'date.format': 'Дата рождения должна быть в формате ГГГГ-ММ-ДД',
+    'date.format': 'Дата должна быть в формате ДД-ММ-ГГГГ',
     'date.max': 'Дата рождения не может быть в будущем',
     'any.required': 'Дата рождения обязательна',
   }),
