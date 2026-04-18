f48cf0d8f25199d9a3235b271c20d2dbca557d24
A <alisazz805@gmail.com>
2026-04-18 23:17:14 +0300

    feat: добавить глобальный exception filter и подключить cors

--- api/src/main.ts
+++ api/src/main.ts
@@ -1,9 +1,13 @@
-import { NestFactory } from '@nestjs/core';
+import { HttpAdapterHost, NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { AllExceptionsFilter } from './shared/pipes/all-exceptions.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const httpAdapterHost = app.get(HttpAdapterHost);
+  app.useGlobalFilters(new AllExceptionsFilter(httpAdapterHost));
   app.setGlobalPrefix('', { exclude: ['/'] });
+  app.enableCors();
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
