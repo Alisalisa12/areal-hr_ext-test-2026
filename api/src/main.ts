4d38f7921b7aacfb42e4ffe56ebf051ecff88e77
A <alisazz805@gmail.com>
2026-04-18 23:18:41 +0300

    feat: удалить глобальный префикс

--- api/src/main.ts
+++ api/src/main.ts
@@ -6,7 +6,6 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const httpAdapterHost = app.get(HttpAdapterHost);
   app.useGlobalFilters(new AllExceptionsFilter(httpAdapterHost));
-  app.setGlobalPrefix('', { exclude: ['/'] });
   app.enableCors();
   await app.listen(process.env.PORT ?? 3000);
 }
