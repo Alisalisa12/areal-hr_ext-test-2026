69fce851432493d43132c91b874fc922a7ba7d02
A <alisazz805@gmail.com>
2026-04-26 17:20:17 +0300

    feat: добавить поддержку сессий и аутентификацию с помощью Passport

--- api/src/main.ts
+++ api/src/main.ts
@@ -1,9 +1,28 @@
+import session from 'express-session';
 import { HttpAdapterHost, NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { AllExceptionsFilter } from './shared/pipes/all-exceptions.filter';
+import passport from 'passport';
+import { Express } from 'express';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const expressApp = app.getHttpAdapter().getInstance() as Express;
+  expressApp.set('trust proxy', 1);
+  app.use(
+    session({
+      secret: process.env.SESSION_SECRET as string,
+      resave: false,
+      saveUninitialized: false,
+      cookie: {
+        maxAge: 1000 * 60 * 60 * 24,
+        secure: false,
+        httpOnly: true,
+      },
+    }),
+  );
+  app.use(passport.initialize());
+  app.use(passport.session());
   const httpAdapterHost = app.get(HttpAdapterHost);
   app.useGlobalFilters(new AllExceptionsFilter(httpAdapterHost));
   app.enableCors();
